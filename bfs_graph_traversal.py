6d3f8f1189cfe7699737c515d5c9b668215f8790
Cierra Higgins <cierramhiggins@gmail.com>
2018-07-14 15:51:01 -0700

    new challenges

--- bfs_graph_traversal.py
+++ bfs_graph_traversal.py
@@ -1,7 +1,3 @@
-
-# How do you return an object
-# How do you test this 
-
 class Queue(object):
 
     def  __init__(self):
@@ -19,27 +15,27 @@ class Queue(object):
     def size(self):
         return len(self.items)
 
-class InstagramAccount(object):
+class SocialMediaAccount(object):
 
-    def __init__(self, instagram_id, username, biography, follower_count, folloewrs):
+    def __init__(self, user_id, username, biography, follower_count, followers):
         self.instagram_id = instagram_id
         self.username = username
         self.biography = biography
         self.follower_count = follower_count
-        self.followers = [] 
+        self.followers = followers
 
-def crawl_instagram_graph(seed_instagram_id):
+def crawl_social_media_graph(seed_user_id):
 
         to_visit = Queue()
-        to_visit.enqueue(seed_instagram_id)
+        to_visit.enqueue(seed_user_id)
         top_followers = []
         visited = set()
-        visited.add(seed_instagram_id)
+        visited.add(seed_user_id)
 
         while not to_visit.is_empty():
             current = to_visit.dequeue()
             if len(self.followers) > 2 and current not in top_followers:
-                top_followers.append(seed_instagram_id.InstagramAccount)
+                top_followers.append(seed_user_id.SocialMediaAccount)
             else:
                 for follower in current.adjacent - visited:
                     to_visit.enqueue(follower)
@@ -47,13 +43,13 @@ def crawl_instagram_graph(seed_instagram_id):
 
         return top_followers
 
-seed = InstagramAccount("cierra", 1, "chiggins", "hi!", [])
-emily = InstagramAccount("emily", 2, "emzz", "hello", [])
-olivia = InstagramAccount("olivia", 3, "oliva1", "heeey", [])
-joe = InstagramAccount("joe", 4, "jj24", " ", [])
+seed = SocialMediaAccount("cierra", 1, "chiggins", "hi!", [])
+emily = SocialMediaAccount("emily", 2, "emzz", "hello", [])
+olivia = SocialMediaAccount("olivia", 3, "oliva1", "heeey", [])
+joe = SocialMediaAccount("joe", 4, "jj24", " ", [])
 seed.followers = [emily, olivia, joe]
 emily.followers = [olivia, seed]
 olivia.followers = [emily, seed]
 joe.followers = [olivia]
-seed_instagram_id = seed.instagram_id
-crawl_instagram_graph(seed_instagram_id)
+seed_user_id = seed.user_id
+crawl_social_media_graph(seed_user_id)
