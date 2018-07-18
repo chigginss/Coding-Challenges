6409061679c536c44eae4585535d8ae5b232629a
Cierra Higgins <cierramhiggins@gmail.com>
2018-07-18 16:35:59 -0700

    new challenges

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,18 @@
 """Binary Search"""
+
+def binary_search(array, target):
+
+    lower = 0
+    upper = len(array)
+
+    while lower < upper:
+        guess = lower + (upper - lower) / 2
+        val = array[guess]
+        if target == val:
+            return guess
+        elif target > val:
+            if lower == guess:
+                break
+            lower = guess
+            elif target < val:
+                upper = guess
