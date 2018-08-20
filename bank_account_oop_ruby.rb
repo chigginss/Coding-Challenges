669260103e1441b450f2408b8c72ae5be1996615
Cierra Higgins <cierramhiggins@gmail.com>
2018-08-20 16:52:02 -0700

    ruby practice oop

--- bank_account_oop_ruby.rb
+++ bank_account_oop_ruby.rb
@@ -1,1 +1,16 @@
-""" Create a bank account class with checking and saving accounts. Add methods to deposite, withdraw, check balance, transfer and trak how many accounts the bank has """
+""" 
+Create a bank account class with checking and saving accounts. 
+Add methods to deposite, withdraw, check balance, transfer and track how many accounts the bank has 
+
+practice with Ruby OOP 
+
+"""
+
+class bankAccount
+
+    private 
+    @@number_of_accounts = 0
+
+    attr_accesor: account_number, checking, saving
+
+    public
