9141321514845672fc57f592486479eef14c14d6
Cierra Higgins <cierramhiggins@gmail.com>
2018-08-20 18:44:59 -0700

    ruby practice oop bank account

--- bank_account_oop_ruby.rb
+++ bank_account_oop_ruby.rb
@@ -9,8 +9,21 @@ practice with Ruby OOP
 class bankAccount
 
     private 
+    
     @@number_of_accounts = 0
+    @@interest_rate = 1.25
 
     attr_accesor: account_number, checking, saving
-
+    
+    def create_account_number
+        rand(10**16)
+    end
+   
     public
+
+    def initialize (checking_deposit, saving_deposit)
+        @account_number = create_account_number
+        @checking = checking_deposit
+        @savings = saving_deposit
+    end
+
